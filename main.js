f74b18dbef269b20769eb8f238045fd869dae73b
lucia vilar <vp_lucia@hotmail.com>
2021-07-06 21:40:02 +0200

    initial commit cleaned

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let ctx = canvas.getContext("2d")
 let splashScreen = document.querySelector("#splash-screen")
 let gameoverScreen = document.querySelector("#gameover-screen")
 let goOnButton = document.querySelector("#start-btn")
-let restartButton = document.querySelector("#restart-btn")
+let playAgainButton = document.querySelector("#restart-btn")
 
 //main game global variable
 let game;
@@ -19,21 +19,5 @@ let game;
 
 goOnButton.addEventListener("click", () => {
     canvas.style.display = "block";
-    splashScreen.style.display = "none"
-
-    // next thing I have to do after push start bottom is create the game. If not I cannot start the game
-    //create gane based on the gameLoop we´ve did
-    gameObj = new Game() // game is the obect we create and will have all properties and methods of Game class!
-        //start game , so what we want is to execute de gameLoop method
-    gameObj.gameLoop() //invoke the method
-
-}) canvas.addEventListener("click", () => {
-    // how I access the method which is in the bird, to the game?
-    gameObj.bird.birdJump() //target is gameObj, that is an objet of game class with all the properties of the class and inside is bird class with all the properties and methods.
-
-}) restartButton.addEventListener("click", () => {
-    canvas.style.display = "block";
-    gameoverScreen.style.display = "none" //copy the same of above but changing splashScreen by gameOverScreen
-    gameObj = new Game();
-    gameObj.gameLoop();
+    splashScreen.style.display = "none";
 })
